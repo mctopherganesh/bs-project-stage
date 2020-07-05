25d12b56b5d11ad54d9309b0c839c241bbdacadf
Christopher Ganesh McMahon <mctopherganesh@gmail.com>
2020-07-05 08:01:03 -0500

    tests connection to db(2)

--- pg_helper.py
+++ pg_helper.py
@@ -1,14 +1,16 @@
 import os
 import psycopg2
 
-db_url = os.environ['heroku_db_url']
+db_url = os.environ['DATABASE_URL']
+
+def connect_to_db():
+    conn = psycopg2.connect(db_url, sslmode='require')
+    cursor = conn.cursor()
+    print('connected tp ' + cursor.fetchone())
+    cursor.close()
+    conn.close()
 
 
-conn = psycopg2.connect(db_url, sslmode='require')
-cursor = conn.cursor()
-print('connected tp ' + cursor.fetchone())
-cursor.close()
-conn.close()
 
 
    
