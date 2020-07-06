6e9ed37a3e7ece4ca1c3d13bbec6b0aadf07770d
Christopher Ganesh McMahon <mctopherganesh@gmail.com>
2020-07-06 09:12:43 -0500

    bad vars number dos

--- data_transformer.py
+++ data_transformer.py
@@ -7,8 +7,8 @@ def transform_data():
     cur, conn = pg_helper.open_connect_to_db()
     cur.execute('select * from bs_test')
     raw_sql_list = cur.fetchall()
-    raw_df = pd.DataFrame(raw_sql_list)
-    raw_df.rename({0 : 'datetime', 1 : 'bs_measure'}, axis=1, inplace=True)
-    trimmed_df = raw_df.set_index('datetime',inplace=True)
-    print(trimmed_df, flush=True)
-    return trimmed_df
+    df = pd.DataFrame(raw_sql_list)
+    df.rename({0 : 'datetime', 1 : 'bs_measure'}, axis=1, inplace=True)
+    df.set_index('datetime',inplace=True)
+    print(df, flush=True)
+    return df
