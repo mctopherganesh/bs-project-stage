994a0e1c6aa7d52d39d1abe1a5cc36a4f93d91d7
Christopher Ganesh McMahon <mctopherganesh@gmail.com>
2020-07-06 19:10:00 -0500

    lets be honest, it's all bugs, all the way down

--- pg_helper.py
+++ pg_helper.py
@@ -16,7 +16,7 @@ def close_connect_to_db(cursor, conn):
 def enter_bs_row(bs,dtsmp):
     cur, conn = open_connect_to_db()
     insert_statement = """insert into bs_test(datetime, bs_measure)
-                          values ({}, {});""".format(dtsmp, bs)
+                          values ('{}', {});""".format(dtsmp, bs)
     cur.execute(insert_statement)
     conn.commit()
     cur.close()
